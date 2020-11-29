9bb26abde43b37d694bf6405d5f68b976e3f6cc1
Willyan (Shizu) <willshizu@gmail.com>
2020-11-29 07:33:24 -0300

    Menssagem de erro atualizada

--- site-feira-escola/php/connect.php
+++ site-feira-escola/php/connect.php
@@ -4,4 +4,4 @@ $user = "root";
 $pw = "";
 $db = "feiraAdm";
 
-$connect = mysqli_connect($host, $user, $pw, $db);
+$connect = mysqli_connect($host, $user, $pw, $db) or die("Não foi possível conectar ao servidor!");
